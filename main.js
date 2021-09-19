99feda083adc05af4b4604f50487ef0cc559134a
Shubhak5 <83910260+Shubhak5@users.noreply.github.com>
2021-09-19 23:20:28 +0530

    ADV_C105_FaceRecognition_Project
    Added code to identify the face from the model

--- main.js
+++ main.js
@@ -15,8 +15,23 @@ function take_snapshot() {
     });
 }
 
-classifier = ml5.classifier('https://teachablemachine.withgoogle.com/models/9M_ge5_Me/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/9M_ge5_Me/model.json', modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded !");
+}
+
+function Check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, getResult);
+}
+
+function getResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
